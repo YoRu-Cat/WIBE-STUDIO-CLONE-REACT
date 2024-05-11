38f697cfa297e35a061bdec05467508165f3b460
YoRu Tech <tech.niko.coding@gmail.com>
2024-05-11 13:05:33 +0500

    Shop page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import "locomotive-scroll/dist/locomotive-scroll.css";
 import Home from "./sections/Home";
 import { AnimatePresence } from "framer-motion";
 import About from "./sections/About";
+import Shop from "./sections/Shop";
+import ScrollTriggerProxy from "./components/ScrollTriggerProxy";
 
 function App() {
   const containerRef = useRef(null);
@@ -28,10 +30,12 @@ function App() {
           }
           containerRef={containerRef}
         >
+          <ScrollTriggerProxy />
           <AnimatePresence>
-            <main data-scroll-container ref={containerRef}>
+            <main className="App" data-scroll-container ref={containerRef}>
               <Home />
               <About />
+              <Shop />
             </main>
           </AnimatePresence>
         </LocomotiveScrollProvider>
